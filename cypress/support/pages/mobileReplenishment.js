8067fefe3c486c9f3fc52e252b1c417f984b2170
Maxymishin <maxymishin@gmail.com>
2021-12-14 23:33:29 +0200

    transfers

---
+++ cypress/support/pages/mobileReplenishment.js
@@ -0,0 +1,47 @@
+export class MobilePhoneReplenishment {
+    typePhoneNumber(phoneNumber){
+        cy.get('[data-qa-node="phone-number"]')
+            .type(phoneNumber)
+    }
+
+    typeAmout(amount){
+        cy.get('[data-qa-node="amount"]')
+            .type(amount)
+    }
+
+    typeDebitCard(cardNumber, exDate, cvv, firstName, lastName){
+        cy.get('[data-qa-node="numberdebitSource"]')
+            .type(cardNumber)
+            .get('[data-qa-node="expiredebitSource"]')
+            .type(exDate)
+            .get('[data-qa-node="cvvdebitSource"]')
+            .type(cvv)
+            .wait(1000)
+            .get('[data-qa-node="firstNamedebitSource"]')
+            .type(firstName)
+            .get('[data-qa-node="lastNamedebitSource"]')
+            .type(lastName)
+    }
+    
+    submitPayment(){
+        cy.get('[data-qa-node="submit"]')
+            .click()
+    }
+
+    checkDebitCard(debitCard){
+        cy.get('[data-qa-node="card"]')
+            .should('have.text', debitCard)
+    }
+
+    checkMobileOperetorName(operatorName){
+        cy.get('[data-qa-node="nameB"]')
+            .should('have.text', operatorName)
+    }
+
+    checkСurrency(currency){
+        cy.get('[data-qa-node="amount"]')
+            .should('contain.text', currency)
+    }
+}
+
+export const mobileReplenishment = new MobilePhoneReplenishment()
---
+++ cypress/support/pages/transfer.js
@@ -0,0 +1,52 @@
+export class Transfers {
+    typeDebitCardData(cardNumber, exDate, cvv, firstName, lastName){
+        cy.get('[data-qa-node="numberdebitSource"]')
+            .type(cardNumber)
+            .get('[data-qa-node="expiredebitSource"]')
+            .type(exDate)
+            .get('[data-qa-node="cvvdebitSource"]')
+            .type(cvv)
+            .wait(1000)
+            .get('[data-qa-node="firstNamedebitSource"]')
+            .type(firstName)
+            .get('[data-qa-node="lastNamedebitSource"]')
+            .type(lastName)
+    }
+
+    typeReceiverCardData(cardNumber, firstName, lastName){
+        cy.get('[data-qa-node="numberreceiver"]')
+        .type(cardNumber)
+        .wait(1000)
+        .get('[data-qa-node="firstNamereceiver"]')
+        .type(firstName)
+        .get('[data-qa-node="lastNamereceiver"]')
+        .type(lastName)
+    }
+
+    typeAmout(amount){
+        cy.get('[data-qa-node="amount"]')
+            .type(amount)
+    }
+    submitPayment(){
+        cy.get('button[type="submit"]')
+            .click()
+    
+    }
+
+    checkDebitAndReceiverCard(debitCard, receiverCard){
+        cy.get('[data-qa-node="payer-card"]')
+            .should('have.text', debitCard)
+            .get('[data-qa-node="receiver-card"]')
+            .should('have.text', receiverCard)
+    }
+    chaeckPayerCurrency(payerCurrency){
+       cy.get('[data-qa-node="payer-currency"]')
+            .should('contain.text', payerCurrency)
+    }
+
+    checkTotalPrice(total){
+        cy.get('[data-qa-node="total"]')
+            .should('contain.text', total)
+    }
+}
+export const transfers = new Transfers()
